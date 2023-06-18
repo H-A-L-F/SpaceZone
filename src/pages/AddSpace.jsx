e1856896ff8407b3a747d35dfc21de87e1c46352
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 03:46:48 +0700

    Create AddSpace.jsx

---
+++ src/pages/AddSpace.jsx
@@ -0,0 +1,12 @@
+import { Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const AddSpace = () => {
+  return (
+    <Flex flexDir="column">
+        <Heading>Add New Space</Heading>
+    </Flex>
+  )
+}
+
+export default AddSpace
