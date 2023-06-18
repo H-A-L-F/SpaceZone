a66537f651b23718d61f1ddb5de07ea052e27bb8
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 00:51:28 +0700

    navbar in master layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import MasterLayout from './layouts/MasterLayout';
 
 
 function App() {
   return (
     <>
+    <MasterLayout>
+      
+    </MasterLayout>
     </>
   );
 }
--- src/layouts/MasterLayout.jsx
+++ src/layouts/MasterLayout.jsx
@@ -1,9 +1,21 @@
+import { Box, Button, ButtonGroup, Flex, Heading, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 const MasterLayout = () => {
-  return (
-    <div>MasterLayout</div>
-  )
+    return (
+        <>
+            <Flex minWidth='max-content' bg="" alignItems='center' gap='2'>
+                <Box p='2'>
+                    <Heading size='md'>Chakra App</Heading>
+                </Box>
+                <Spacer />
+                <ButtonGroup gap='2'>
+                    <Button colorScheme='teal'>Sign Up</Button>
+                    <Button colorScheme='teal'>Log in</Button>
+                </ButtonGroup>
+            </Flex>
+        </>
+    )
 }
 
 export default MasterLayout
