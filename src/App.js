8ae90e25c83fc2d0073f061db7a2f1c08b102547
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-18 23:50:52 +0700

    init setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  theme,
-} from '@chakra-ui/react';
+
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-    </ChakraProvider>
+    <>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  ChakraProvider,
+  theme,
+  ColorModeScript,
+} from '@chakra-ui/react';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>
 );
