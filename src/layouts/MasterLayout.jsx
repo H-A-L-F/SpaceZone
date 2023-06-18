f516935e05a4af6bf0df888bbdc67c0f2b894420
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 01:52:47 +0700

    Working layout component

--- src/layouts/MasterLayout.jsx
+++ src/layouts/MasterLayout.jsx
@@ -4,15 +4,20 @@ import React from 'react'
 const MasterLayout = () => {
     return (
         <>
-            <Flex minWidth='max-content' bg="primary.500" alignItems='center' gap='2'>
-                <Box p='2'>
-                    <Heading size='md'>Chakra App</Heading>
+            <Flex direction="column" bg="quartery.500" height="100vh">
+                <Flex minWidth='full' bg="tertiary.500" alignItems='center' gap='2'>
+                    <Box p='2'>
+                        <Heading size='md'>Chakra App</Heading>
+                    </Box>
+                    <Spacer />
+                    <ButtonGroup gap='2'>
+                        <Button colorScheme='teal'>Sign Up</Button>
+                        <Button colorScheme='teal'>Log in</Button>
+                    </ButtonGroup>
+                </Flex>
+                <Box flex="1" p={4}>
+                    Body
                 </Box>
-                <Spacer />
-                <ButtonGroup gap='2'>
-                    <Button colorScheme='teal'>Sign Up</Button>
-                    <Button colorScheme='teal'>Log in</Button>
-                </ButtonGroup>
             </Flex>
         </>
     )
