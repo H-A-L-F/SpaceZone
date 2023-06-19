bc6a88be62cdbe05e75413af08b5af3f9a3e036f
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 08:22:39 +0700

    implwement auth layout

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import AddSpace from './pages/AddSpace';
+import AuthLayout from './layouts/AuthLayout';
+import Register from './pages/Register';
 
 
 function App() {
   return (
     <Routes>
+      <Route path='auth' element={<AuthLayout />}>
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Home />} />
+      </Route>
       <Route path='/' element={<MasterLayout />}>
-        <Route path='home' element={<Home />}/>
+        <Route path='home' element={<Home />} />
         <Route path='space'>
-          <Route path='add' element={<AddSpace />}/>
+          <Route path='add' element={<AddSpace />} />
         </Route>
       </Route>
-      <Route path='*' element={<NotFound />}/>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
