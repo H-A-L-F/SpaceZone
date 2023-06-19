960dab945a8e26e68a0ae5cc791e90fb15c2d5d9
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 09:10:02 +0700

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { Box, Button, Container, Flex, FormControl, FormLabel, Heading, Input, Spacer } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = () => {
+    console.log(email)
+    console.log(password)
+  }
+
   return (
-    <div>Register</div>
+    <Flex flexDir="column" gap="6" paddingX="8" paddingY="6" h="full">
+      <Heading color="primary.700">Register</Heading>
+      <Box marginTop="2" />
+      <FormControl isRequired>
+        <FormLabel color="primary.700" >Email</FormLabel>
+        <Input
+          color="primary.700"
+          colorScheme='primary'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel color="primary.700" >Password</FormLabel>
+        <Input
+          color="primary.700"
+          colorScheme='primary'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Password'
+        />
+      </FormControl>
+      <Spacer />
+      <Button onClick={handleSubmit} colorScheme='primary' variant='solid' w="sm" alignSelf="center">
+        Submit
+      </Button>
+    </Flex>
   )
 }
 
