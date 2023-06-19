f176170114d7ce3f6e09a689e8502ed0f4711839
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 14:28:25 +0700

    Create Space.js

---
+++ src/models/Space.js
@@ -0,0 +1,9 @@
+export function constructSpace(name, desc, price, photoURL) {
+    const space = {
+        name: name,
+        desc: desc,
+        price: price,
+        photoURL: photoURL
+    }
+    return space
+}
