30dd9d4ee42d8ee23d18cde599f70427bfc1b9ed
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 21:05:52 +0700

    orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSnapCollection } from '../lib/UseSnapCollection'
 import { collection, doc, query, where } from 'firebase/firestore'
 import { FB_DB } from '../lib/Firebase'
 import { useUserAuth } from '../lib/AuthContext'
+import { Flex, Heading, Wrap, WrapItem } from '@chakra-ui/react'
+import { FIRESTORE_FETCH_LOADING } from '../actions/UseSnapCollection'
+import SpaceCardSkele from '../components/SpaceCardSkele'
+import BookingCard from '../components/BookingCard'
 
 const Orders = () => {
 
     const { user } = useUserAuth()
 
-    const userRef = doc(FB_DB, "user", user.id)
-    const ordersState = useSnapCollection(query(collection(FB_DB, "booking"), where("ownerRef", "==", userRef)))
+    const ordersState = useSnapCollection(query(collection(FB_DB, "booking"), where("ownerRef", "==", user.path)))
+
+    useEffect(() => {
+        console.log(ordersState)
+
+    }, [ordersState])
 
     return (
-        <div>Orders</div>
+        <Flex flexDir="column" color="primary.700" gap="4">
+            <Heading>Browse Spaces</Heading>
+            <Wrap spacing='30px'>
+                {
+                    ordersState.status === FIRESTORE_FETCH_LOADING ?
+                        Array.from({ length: 12 }, (_, i) => i + 1).map((id) =>
+                            <WrapItem key={id}>
+                                <SpaceCardSkele />
+                            </WrapItem>
+                        ) :
+                        ordersState.data.map((b) =>
+                            <WrapItem key={b.id}>
+                                <BookingCard booking={b} />
+                            </WrapItem>
+                        )
+                }
+            </Wrap>
+        </Flex>
     )
 }
 
