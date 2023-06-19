d0c44b6d808ab78cc161bd678484694d6a49d6f7
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-19 21:34:42 +0700

    status for bookings

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -1,7 +1,8 @@
-import { Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Badge, Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from '@chakra-ui/react'
 import { getDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import SpaceCardSkele from './SpaceCardSkele'
+import { STATUS_COMPLETED, STATUS_ONGOING, STATUS_PENDING } from '../models/Booking'
 
 const BookingCard = ({ booking }) => {
     const [space, setSpace] = useState()
@@ -17,6 +18,16 @@ const BookingCard = ({ booking }) => {
             })
     }, [booking.spaceRef])
 
+    function getColor() {
+        if(booking.status === STATUS_PENDING) return "yellow"
+        if(booking.status === STATUS_ONGOING) return "blue"
+        if(booking.status === STATUS_COMPLETED) return "green"
+    }
+
+    function handleConfirm() {
+
+    }
+
     if (!space) return <SpaceCardSkele />
     else return (
         <Card
@@ -25,6 +36,8 @@ const BookingCard = ({ booking }) => {
             variant='outline'
         >
             <Image
+                w="400px"
+                h="250px"
                 objectFit='cover'
                 maxW={{ base: '100%', sm: '200px' }}
                 src={space.photoURL}
@@ -33,9 +46,14 @@ const BookingCard = ({ booking }) => {
 
             <Stack>
                 <CardBody>
-                    <Heading size='md'>{space.name}</Heading>
+                    <Heading size='md'>
+                        {space.name}
+                        <Badge ml='1' variant='solid' colorScheme={getColor()}>
+                            {booking.status}
+                        </Badge>
+                    </Heading>
 
-                    <Text py='2'>
+                    <Text py='2' maxW={48} noOfLines={3}>
                         {space.desc}
                     </Text>
                 </CardBody>
@@ -44,6 +62,7 @@ const BookingCard = ({ booking }) => {
                     <Button variant='solid' colorScheme='blue'>
                         Confirm
                     </Button>
+
                 </CardFooter>
             </Stack>
         </Card>
