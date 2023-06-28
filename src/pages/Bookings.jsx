e6318636d8851e0f8a1ff60eff635aa99fba004a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-06-28 20:33:26 +0700

    booking empty handler

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -4,24 +4,26 @@ import { collection, doc, query, where } from 'firebase/firestore'
 import { FB_DB } from '../lib/Firebase'
 import { useUserAuth } from '../lib/AuthContext'
 import { Flex, Heading, Wrap, WrapItem } from '@chakra-ui/react'
-import { FIRESTORE_FETCH_LOADING } from '../actions/UseSnapCollection'
+import { FIRESTORE_FETCH_LOADING, FIRESTORE_FETCH_SUCCESS } from '../actions/UseSnapCollection'
 import SpaceCardSkele from '../components/SpaceCardSkele'
 import BookingCard from '../components/BookingCard'
 import { STATUS_COMPLETED, STATUS_ONGOING, STATUS_PENDING } from '../models/Booking'
+import { useLoadingContext } from '../lib/LoadingContext'
 
 const Bookings = () => {
     const { user } = useUserAuth()
+    const {setLoading} = useLoadingContext() 
 
     const bookingsState = useSnapCollection(query(collection(FB_DB, "booking"), where("userRef", "==", doc(FB_DB, "user", user.id))))
 
-    console.log(user.path)
 
-    useEffect(() => {
-        console.log(bookingsState)
+    // useEffect(() => {
+    //     if(!bookingsState.data) setLoading(true)
+    //     else setLoading(false)
 
-    }, [bookingsState])
+    // }, [bookingsState.data, setLoading])
 
-    // if(!bookingsState.data) return <Heading color="primary.700">Empty...</Heading>
+    if(bookingsState.status === FIRESTORE_FETCH_SUCCESS && bookingsState.data.length <= 0) return <Heading color="primary.700">You have no booking...</Heading>
     return (
         <Flex flexDir="column" color="primary.700" gap="4">
             <Heading>Bookings</Heading>
